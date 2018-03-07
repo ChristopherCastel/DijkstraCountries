810caeae69516078b973f043944fa50c3d6cb2d6
martoufff <martoufff@hotmail.com>
2018-03-07 07:48:29 +0100

    Init projet

---
+++ src/SAXHandler.java
@@ -0,0 +1,16 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXHandler extends DefaultHandler {
+
+  @Override
+  public void startElement(String uri, String localName, String qName, Attributes attributes)
+      throws SAXException {}
+
+  @Override
+  public void endElement(String uri, String localName, String qName) throws SAXException {}
+
+  @Override
+  public void characters(char ch[], int start, int length) throws SAXException {}
+}
